9e9afd56e7cd79040c7acdc47147c04fc3c8297d
Hasan07 <Jahidhasan2856@gmail.com>
2021-06-04 23:06:18 +0600

    hungry monster website develop complete

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//search meal button handle
 const searchMeal = () => {
   const inputMealName = document.getElementById("meal-name").value;
   if (inputMealName.length == 0) {
@@ -10,6 +11,7 @@ const searchMeal = () => {
   document.getElementById("meal-section").innerHTML = "";
 };
 
+//fetch meal by filter ingredient, api call
 const getMealData = (mealName) => {
   const api = "https://www.themealdb.com/api/json/v1/1/filter.php?";
   fetch(`${api}i=${mealName}`)
@@ -17,29 +19,42 @@ const getMealData = (mealName) => {
     .then((data) => createUI(data));
 };
 
+// create multi meal UI section and show meal
 const createUI = (data) => {
-  const mealData = data.meals.slice(0, 8);
-  console.log(mealData);
   const mealArea = document.getElementById("meal-section");
-  for (let i = 0; i < mealData.length; i++) {
-    const meal = mealData[i];
-    const createBox = document.createElement("div");
-    createBox.classList.add("col-3");
-    createBox.innerHTML = `
-                            <div onclick="mealDetails(${meal.idMeal})" class="card food-area mb-4">
-                            <img src="${meal.strMealThumb}" />
-                            <h3 class="card-footer p-3">${meal.strMeal}</h3> 
-                            </div>
-                            `;
-    mealArea.appendChild(createBox);
+  if (data.meals == null) {
+    const unknown = document.createElement("h1");
+    unknown.classList.add("text-center", "text-danger");
+    unknown.innerHTML = "Unknown meal !!";
+    mealArea.appendChild(unknown);
+  } else {
+    const mealData = data.meals.slice(0, 8);
+
+    for (let i = 0; i < mealData.length; i++) {
+      const meal = mealData[i];
+      const createBox = document.createElement("div");
+      createBox.classList.add("col-3");
+      createBox.innerHTML = `
+                              <div onclick="mealDetails(${meal.idMeal})" class="card food-area mb-4">
+                              <img src="${meal.strMealThumb}" />
+                              <h3 class="card-footer p-3">${meal.strMeal}</h3> 
+                              </div>
+                              `;
+      mealArea.appendChild(createBox);
+    }
   }
 };
-const mealDetails = (id) => {
-  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+//handle meal detail button
+
+const mealDetails = (idMeal) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => creatMealDetailUI(data.meals[0]));
 };
+
+// create meal detail UI
 const creatMealDetailUI = (data) => {
   document.getElementById("show-search-meal").style.display = "none";
   const meal = document.getElementById("meal");
